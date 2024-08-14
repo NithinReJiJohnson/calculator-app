4d1b3447a930e732ae934c59458077106876a262
NRJohnson <nithinrejijohnsonpython@gmail.com>
2024-08-14 12:59:52 +0100

    Added division feature

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from calc_functions import do_addition, do_subtraction, do_multiplication
+from divide import do_division
 
 def main():
     print('Welcome to the calculator app')
     print("""select the function from the given options:
           1.Add
           2.Subtract
+          3. Divide
           """)
     user_input=input("select the option:")
     a=int(input('Value of A:'))
@@ -15,7 +17,11 @@ def main():
         print(result)
     elif user_input=="2":
         result1=do_subtraction(a,b)
-        print(result1) 
+        print(result1)
+
+    elif user_input=="3":
+        result2=do_division(a,b)
+        print(result2) 
 
     else:
         result2=do_multiplication(a,b)
---
+++ divide.py
@@ -0,0 +1,4 @@
+# Division
+
+def do_division(a,b):
+    return a/b
