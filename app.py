097a77cb6e7feb2cb587880cf6be0a8c7f4d86ac
JohnsonNithin <nithinrejijohnson@gmail.com>
2024-09-27 13:21:23 +0100

    changes made to only include add and subtract

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from calc_functions import do_addition, do_subtraction, do_multiplication
-from divide import do_division 
-# from area import calculate_area_rectangle #> You can also import this line but for this you have to create area .py
-from calc_functions import calculate_area_rectangle
+from calc_functions import do_addition, do_subtraction
+
 
 
 def main():
@@ -9,8 +7,6 @@ def main():
     print("""select the function from the given options:
           0.Add
           1.Subtract
-          2. Multiply
-          3.Divide
           """)
     user_input=input("select the option:")
     a=int(input('Value of A:'))
@@ -21,22 +17,12 @@ def main():
         result=do_addition(a,b)
         print(result)
         
-    elif user_input=="1":
+    else:
         result1=do_subtraction(a,b)
         print(result1)
 
-    elif user_input=="2":
-        result2=do_multiplication(a,b)
-        print(result2)
     
-    elif user_input=="3":
-        result3=do_division(a,b)
-        print(result3) 
 
-    else:
-        result4=calculate_area_rectangle(a,b)
-        print('Area is :',result4)
-        
         
         
         
