cccfa286746483c50c02375bb2ee16c21dfd6d60
JohnsonNithin <nithinrejijohnson@gmail.com>
2024-09-27 12:45:22 +0100

    added feature for area and division

--- app.py
+++ app.py
@@ -1,31 +1,41 @@
 from calc_functions import do_addition, do_subtraction, do_multiplication
-from divide import do_division
+from divide import do_division 
+# from area import calculate_area_rectangle #> You can also import this line but for this you have to create area .py
+from calc_functions import calculate_area_rectangle
+
 
 def main():
     print('Welcome to the calculator app')
     print("""select the function from the given options:
-          1.Add
-          2.Subtract
-          3. Divide
+          0.Add
+          1.Subtract
+          2. Multiply
+          3.Divide
           """)
     user_input=input("select the option:")
     a=int(input('Value of A:'))
     b=int(input('Value of B:'))
 
-    if user_input=="1":
+
+    if user_input=="0":
         result=do_addition(a,b)
         print(result)
-    elif user_input=="2":
+        
+    elif user_input=="1":
         result1=do_subtraction(a,b)
         print(result1)
 
-    elif user_input=="3":
-        result2=do_division(a,b)
-        print(result2) 
-
-    else:
+    elif user_input=="2":
         result2=do_multiplication(a,b)
         print(result2)
+    
+    elif user_input=="3":
+        result3=do_division(a,b)
+        print(result3) 
+
+    else:
+        result4=calculate_area_rectangle(a,b)
+        print('Area is :',result4)
         
         
         
--- calc_functions.py
+++ calc_functions.py
@@ -9,3 +9,13 @@ def do_subtraction(a:int,b:int):
 
 def do_multiplication(a,b):
     return a*b
+
+def do_division(a,b):
+    try:
+        return a/b
+    except ZeroDivisionError as e :
+        return "Cannot divide by Zero!"
+    
+def calculate_area_rectangle(a,b):
+    return a*b
+
